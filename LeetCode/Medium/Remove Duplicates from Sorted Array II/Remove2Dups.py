ee48fad0582daccff3fcca5a20797d71004bd392
Terrance Myles Jr <tlmyles5@hotmail.com>
2025-01-28 22:45:59 -0600

    Create Remove2Dups.py

---
+++ LeetCode/Medium/Remove Duplicates from Sorted Array II/Remove2Dups.py
@@ -0,0 +1,11 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        j = 1
+        for i in range(1, len(nums)):
+            if j == 1 or nums[i] != nums[j - 2]:
+                nums[j] = nums[i]
+                j += 1
+        return j
+
+s = Solution()
+s.removeDuplicates([1, 1])
