31befa95bdc79a1906bc08a4d9088857ec3143f8
Terrance Myles Jr <tlmyles5@hotmail.com>
2025-04-22 15:19:12 -0500

    Create template.cpp

---
+++ LeetCode/C++_150/template.cpp
@@ -0,0 +1,19 @@
+#include "iostream"
+#include <string>
+#include <vector>
+#include <cmath>
+#include <map>
+#include <cstdlib>
+ 
+using namespace std;
+
+class Solution {
+    public:
+        void someThing() {
+            
+        }
+    };
+
+int main (){
+    return 0;
+}
